844541ef8507609ad50fdf0b7c94f827f030e7b7
pablosingh <pablo22_4@hotmail.com>
2025-10-07 22:56:22 -0300

    Funcion de Editar

--- crud_cuentas_bancarias/main.py
+++ crud_cuentas_bancarias/main.py
@@ -10,6 +10,7 @@ def muestra_menu():
     print("2 - Listar Cuentas Bancarias")
     print("3 - Buscar Cuenta por Numero")
     print("4 - Borrar Cuenta Bancaria")
+    print("5 - Editar Cuenta Bancaria")
 
 def ingresa_opcion():
     while True:
@@ -63,6 +64,43 @@ def borrar_cuenta_bancaria():
         cuentas.pop(indice_cuenta_a_buscar)
         print("Eliminado")
 
+def mostrar_menu_edicion():
+    print("\n===============================")
+    print("Editando...")
+    print("0 - Fin de Edicion")
+    print("1 - Editar Nombre")
+    print("2 - Editar Apellido")
+    print("3 - Editar Tipo")
+    print("4 - Editar Saldo")
+
+def editar_cuenta():
+    cuenta, indice = buscar_cuenta_por_numero()
+    if indice == -1:
+        print("Volviendo al menu principal")
+        return
+    else:
+        nombre = cuenta.nombre
+        apellido = cuenta.apellido
+        tipo = cuenta.tipo
+        saldo = cuenta.saldo
+    while cuenta:
+        mostrar_menu_edicion()
+        opcion = ingresa_opcion()
+        if opcion == 0:
+            cuentas[indice].nombre = nombre
+            cuentas[indice].apellido = apellido
+            cuentas[indice].tipo = tipo
+            cuentas[indice].saldo = saldo
+            print("Guardando")
+            break
+        elif opcion == 1:
+            nombre = input("Ingrese el NUEVO nombre : ")
+        elif opcion == 2:
+            apellido = input("Ingrese el NUEVO apellido : ")
+        elif opcion == 3:
+            tipo = input("Ingrese el NUEVO tipo : ")
+        elif opcion == 4:
+            saldo = pedir_float("Ingrese el NUEVO saldo : ")
 
 def main():
     while True:
@@ -78,6 +116,8 @@ def main():
             buscar_cuenta_por_numero()
         elif opcion == 4:
             borrar_cuenta_bancaria()
+        elif opcion == 5:
+            editar_cuenta()
         else:
             print("Opcion no valida")
 
